74b32c219fc8dd5b4d75225f78f7ec7c07029610
Hlib <55288484+Hlib-mats@users.noreply.github.com>
2022-02-10 17:26:29 +0200

    Initial commit

---
+++ InvestUtility.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SimCorpTask
+{
+    class InvestUtility
+    {
+        private DateTime agreementDate;
+        private DateTime calculationDate;
+        private double x; //base payment
+        private double r; //rate
+
+        public int N
+        {
+            get 
+            { 
+                return CalculationDate.Year - AgreementDate.Year; 
+            }
+        }
+
+        public double R
+        {
+            get { return r; }
+            set { r = value; }
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public DateTime CalculationDate
+        {
+            get { return calculationDate; }
+            set { calculationDate = value; }
+        }
+
+        public DateTime AgreementDate
+        {
+            get { return agreementDate; }
+            set { agreementDate = value; }
+        }
+
+        public InvestUtility( double rate, double basePayment, DateTime calculationDate, DateTime agreementDate)
+        {
+            R = rate;
+            X = basePayment;
+            CalculationDate = calculationDate;
+            AgreementDate = agreementDate;
+        }
+
+        public static double GetProfit(double rate, double basePayment, DateTime calculationDate, DateTime agreementDate)
+        {
+            double profit;
+            double N = calculationDate.Year - agreementDate.Year;
+            double percents = 1 + ((rate/100) / 12);
+            profit = Math.Pow(percents, 12 * N);
+            profit = profit * basePayment;
+            return profit;
+        }
+
+        public double GetProfit()
+        {
+            return GetProfit(R, X, CalculationDate, AgreementDate);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimCorpTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime agreementDate;
+            DateTime calculationDate;
+            double x; 
+            double r;
+
+            GetInfo(out r,out x,out calculationDate,out agreementDate);
+            var investUtility = new InvestUtility(r,x,calculationDate,agreementDate);
+            Console.WriteLine(investUtility.GetProfit());
+        }
+
+        static void GetInfo(out double rate,out double basePayment,out DateTime calculationDate,out DateTime agreementDate)
+        {
+            Console.WriteLine("Enter R(% per year)");
+            double.TryParse(Console.ReadLine(),out rate);
+            Console.WriteLine("Enter base payment");
+            double.TryParse(Console.ReadLine(), out basePayment);
+            Console.WriteLine("Enter agreement date(DD/MM/YYYY)");
+            agreementDate = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Enter calculation date(DD/MM/YYYY)");
+            calculationDate = Convert.ToDateTime(Console.ReadLine());
+        }
+    }
+}
